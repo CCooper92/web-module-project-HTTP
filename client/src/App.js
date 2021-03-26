d59e863b8753cce3672275a37bb237a4afcbddfa
CCooper92 <am10ac17@gmail.com>
2021-03-26 11:05:14 -0700

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import MovieList from './components/MovieList';
 import Movie from './components/Movie';
 
 import MovieHeader from './components/MovieHeader';
-
+import AddFriendForm from './components/AddMovieForm';
 import EditMovieForm from './components/EditMovieForm';
 import FavoriteMovieList from './components/FavoriteMovieList';
 
@@ -26,6 +26,7 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id)=> {
+    setMovies(movies.filter(item=>(item.id !== Number(id))));
   }
 
   const addToFavorites = (movie) => {
@@ -45,10 +46,15 @@ const App = (props) => {
         
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm setMovies={setMovies} />
+            </Route>
+
+            <Route path='/movies/add'>
+              <AddFriendForm setMovies={setMovies}/>
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie}/>
             </Route>
 
             <Route path="/movies">
--- client/src/components/AddMovieForm.js
+++ client/src/components/AddMovieForm.js
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-
 import axios from 'axios';
+import { useHistory, Link } from 'react-router-dom';
 
-const EditMovieForm = (props) => {
+const AddMovieForm = (props) => {
 	const { push } = useHistory();
-	const { id } = useParams();
 
 	const [movie, setMovie] = useState({
 		title:"",
@@ -14,7 +11,7 @@ const EditMovieForm = (props) => {
 		genre: "",
 		metascore: 0,
 		description: ""
-	});
+    });
 	
 	const handleChange = (e) => {
         setMovie({
@@ -23,28 +20,18 @@ const EditMovieForm = (props) => {
         });
 	}
 
-	useEffect(() => {
-		axios.get(`http://localhost:5000/api/movies/${id}`)
-		.then(res => {;
-			setMovie(res.data);
-		})
-		.catch(err=>{
-			console.log(err.response);
-		})
-	},[]);
-
     const handleSubmit = (e) => {
 		e.preventDefault();
 		axios
-		.put(`http://localhost:5000/api/movies/${id}`, movie)
+		.post(`http://localhost:5000/api/movies`, movie)
 		.then(res => {
 			props.setMovies(res.data);
-			push(`/movies/${id}`);
+			push(`/movies`);
 		})
 		.catch(err=> {
 			console.log(err.response)
 		})
-	}
+    }
 	
 	const { title, director, genre, metascore, description } = movie;
 
@@ -53,7 +40,7 @@ const EditMovieForm = (props) => {
 		<div className="modal-content">
 			<form onSubmit={handleSubmit}>
 				<div className="modal-header">						
-					<h4 className="modal-title">Editing <strong>{movie.title}</strong></h4>
+					<h4 className="modal-title">Adding <strong>{movie.title}</strong></h4>
 				</div>
 				<div className="modal-body">					
 					<div className="form-group">
@@ -85,6 +72,7 @@ const EditMovieForm = (props) => {
 			</form>
 		</div>
 	</div>);
-}
+};
 
-export default EditMovieForm;
+
+export default AddMovieForm;
